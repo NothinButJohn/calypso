9458794f2e1bb94f601d093c7e93af081513b8ed
NothinButJohn <jharkins2@wisc.edu>
2021-02-20 17:21:42 -0600

    testing

--- src/app/components/charts/candlestick/candlestick.component.ts
+++ src/app/components/charts/candlestick/candlestick.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { AlphaVantageService, CandlestickChartOptions } from 'src/app/services/alpha-vantage.service';
 import { candlestickChartOptionsSelector } from 'src/app/store/selectors/alpha-vantage.selectors';
@@ -12,10 +12,44 @@ import { candlestickChartOptionsSelector } from 'src/app/store/selectors/alpha-v
 })
 export class CandlestickComponent implements OnInit {
   intradaySeriesData$: Observable<CandlestickChartOptions>
+
+  
+  options: CandlestickChartOptions = {
+    chart: {
+      type: "candlestick",
+      height: 350
+    },
+    series: [],  
+    title: {
+      text: '',
+      align: "left"
+    },
+    xaxis: {
+      type: "datetime"
+    },
+    yaxis: {
+      tooltip: {
+        enabled: true,
+      }
+    },
+    tooltip: {
+      enabled: true,
+      theme: 'dark',
+    },
+    theme: {
+      mode: 'dark'
+    },
+    noData: {
+        text: 'Search for an Equity'
+    }
+  }
+  
   constructor(private alphaVantage: AlphaVantageService,
     private store: Store) { }
 
   ngOnInit(): void {
+    // this.intradaySeriesData$ = of(this.options)
+    
     this.intradaySeriesData$ = this.store.select(candlestickChartOptionsSelector).pipe(
       tap(x => console.log(x))
     )
--- src/app/store/reducers/alpha-vantage.reducers.ts
+++ src/app/store/reducers/alpha-vantage.reducers.ts
@@ -2,6 +2,35 @@ import { Action, createReducer, on } from '@ngrx/store';
 import { CandlestickChartOptions } from 'src/app/services/alpha-vantage.service';
 import * as AlphaActions from '../actions/alpha-vantage.actions'
 
+export const initChartOptions:CandlestickChartOptions = {
+    chart: {
+      type: "candlestick",
+      height: 350
+    },
+    series: [],  
+    title: {
+      text: '',
+      align: "left"
+    },
+    xaxis: {
+      type: "datetime"
+    },
+    yaxis: {
+      tooltip: {
+        enabled: true,
+      }
+    },
+    tooltip: {
+      enabled: true,
+      theme: 'dark',
+    },
+    theme: {
+      mode: 'dark'
+    },
+    noData: {
+        text: 'Search for an Equity'
+    }
+  }
 
 export interface StocksState {
     searchResults: [],
@@ -13,36 +42,9 @@ export const initialStocksState: StocksState = {
     searchResults: [],
     selectedStock: {},
     selectedInterval: '1min',
-    chartOptions: {
-        chart: {
-          type: "candlestick",
-          height: 350
-        },
-        series: [],  
-        title: {
-          text: '',
-          align: "left"
-        },
-        xaxis: {
-          type: "datetime"
-        },
-        yaxis: {
-          tooltip: {
-            enabled: true,
-          }
-        },
-        tooltip: {
-          enabled: true,
-          theme: 'dark',
-        },
-        theme: {
-          mode: 'dark'
-        },
-        noData: {
-            text: 'Search for an Equity'
-        }
-      }
+    chartOptions: initChartOptions
 }
+
 
 export const stocksReducer = createReducer<StocksState>(
     initialStocksState,
